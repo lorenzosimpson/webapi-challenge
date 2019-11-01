93f2a465159e55d610b193b356e041f6057b6a7a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-01 12:15:42 -0400

    completes MVP

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -5,12 +5,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     actionDb.get()
-    .then(actions => {
-        res.status(200).json(actions)
-    })
+    .then(actions => res.status(200).json(actions))
     .catch(err => res.status(500).json({ error: "Failed to get actions" }))
 })
-
 
 router.post('/', (req, res) => {
     const newAction = req.body;
@@ -34,9 +31,7 @@ router.put('/:id', (req, res) => {
     const id = req.params.id;
     const toUpdate = req.body;
     actionDb.update(id, toUpdate)
-    .then(updated => {
-        res.status(200).json(updated)
-    })
+    .then(updated => res.status(200).json(updated))
     .catch(err => res.status(500).json({ error: "Failed to update action" }))
 })
 
